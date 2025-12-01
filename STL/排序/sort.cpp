a1b94e9717f7e0df886c484e38e5de7d50aace65
Dencign <1653648340@qq.com>
2025-12-01 23:28:42 +0800

    12.1

---
+++ STL/排序/sort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int>arr={30,20,50,10,40,60};
+    //sort(*开始地址，*结束地址后一位,排序方法cmp) 默认从小到大 内核是快排
+    sort(arr.begin(),arr.end());
+    //排序逻辑 "a 应该排在 b 前面吗？" 如果 a < b 为 true，回答"是"，就把 a 放前面
+    for(auto x:arr){
+        cout<<x<<' ';
+    }
+    cout<<endl;
+    vector<int>arr1={30,20,50,10,40,60,60};
+    //less “<”运算符；greater “>”运算符
+    sort(arr1.begin(),arr1.end(),greater<int>());
+    
+    for(auto x:arr1){
+        cout<<x<<' ';
+    }
+    cout<<'\n';
+    //去重
+    arr1.erase(unique(arr1.begin(),arr1.end()),arr1.end());
+    for(auto x:arr1){
+        cout<<x<<' ';
+    }
+}
---
+++ 动态规划/数的种类(存在性dp).cpp
@@ -0,0 +1,61 @@
+//给定n个整数，问由这些整数通过“加法”操作，可以组成多少种数字？
+
+#include<iostream>
+using namespace std;
+
+const int N = 5e3 + 10,M= 5e5 + 10; 
+int a[N];
+bool dp[M];
+/*
+const int N = 5e3 + 10;
+int a[N];
+bool dp[N][N];
+
+void solve(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+        sum+=a[i];
+    }   
+
+    dp[0][0]=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<N;j++){
+            dp[i+1][j]=dp[i][j];
+        }
+        for(int j=0;j<N;j++){
+            dp[i+1][j+a[i+1]]=dp[i][j];
+        }
+    }
+    int count=0;
+    for(int i=0;i<=N;i++){
+        if(dp[n][i])
+            count++;
+    }
+    cout<<count;
+}
+*/
+void solve(int n){
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+    }   
+
+    dp[0]= true;
+    for(int i = 1;i <= n;i++){
+        for(int j=5e5;j >= a[i];j--){
+            dp[j] |= dp[j-a[i]];
+        }
+    }
+    long long count=0;
+    for(int i=0;i<=5e5;i++){
+        if(dp[i])
+            count++;
+    }
+    cout<<count<<'\n';
+}
+
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    int n;cin>>n;
+    solve(n);
+}
